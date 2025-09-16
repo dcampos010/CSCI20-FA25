e6fd53040e8c2bd0f7c7df2795fea3ebb232d439
dcampos010 <dcampos010@student.butte.edu>
2025-09-16 20:22:12 +0000

    Gay!

---
+++ Project/project.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    bool b=true;
+    // boolean value peepee
+    int x = 5;
+    std::printf("%i",x);
+    std::printf("%i",b);
+    // It doesn't matter! I'm gay! 
+    std::printf("hello world");
+    return 0;
+}
